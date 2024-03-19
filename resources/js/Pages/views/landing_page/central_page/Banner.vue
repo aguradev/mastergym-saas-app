448ae76caa7d19be4e125d565f816fcb4b873d9c
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2024-03-19 14:44:16 +0800

    update rasponsive navbar from pc - unfinished

---
+++ resources/js/Pages/views/landing_page/central_page/Banner.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1>This is The Banner</h1>
+    </div>
+</template>
--- resources/js/Pages/views/landing_page/central_page/MainMenu.vue
+++ resources/js/Pages/views/landing_page/central_page/MainMenu.vue
@@ -1,11 +1,16 @@
 <script setup>
     import { Head } from '@inertiajs/vue3';
     import MainNavLayout from '@/layouts/MainNavLayout.vue';
+    import Banner from './Banner.vue';
+    import Button from 'primevue/button';
+
+    import { ref } from 'vue';
 </script>
 
 <template>
     <MainNavLayout>
         <Head title="Main Landing Page" />
-        <h1>Test</h1>
+        <div class="bg-cyan-500 h-[70px]"></div>
+        <Banner />
     </MainNavLayout>
 </template>
