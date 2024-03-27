22dd524ec9e15acd85098322af0978010084b911
aguradev <wahmayun.wm@gmail.com>
2024-03-27 21:21:44 +0800

    setup roles and permissions packages

---
+++ app/Models/CentralModel/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\CentralModel;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Model
+{
+    use HasFactory, HasRoles;
+
+    protected $table = "central_users";
+}
--- config/app.php
+++ config/app.php
@@ -168,7 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TenancyServiceProvider::class
+        App\Providers\TenancyServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class
     ])->toArray(),
 
     /*
