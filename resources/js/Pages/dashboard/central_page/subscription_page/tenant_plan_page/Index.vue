1411d63cdaf38d5ad193853d501b05fe704a057f
gungrages <gungrages>
2024-06-08 17:39:07 +0800

    feat: add transition tenant detail to new version form

--- resources/js/Pages/dashboard/central_page/subscription_page/tenant_plan_page/Index.vue
+++ resources/js/Pages/dashboard/central_page/subscription_page/tenant_plan_page/Index.vue
@@ -1,56 +1,93 @@
 <script setup>
-import { defineAsyncComponent, onMounted, reactive, ref, toRef } from 'vue'
-import PlanTenantsLayout from '@layouts/PlanTenantsLayout.vue';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ActionLists from '@components/elements/ulLists/ActionLists.vue';
-import PrimaryButton from '@components/elements/button/PrimaryButton.vue';
-import Modal from '@components/ui/modal/Index.vue';
+import {
+    defineAsyncComponent,
+    onMounted,
+    reactive,
+    ref,
+    toRef,
+    watch,
+} from "vue";
+import PlanTenantsLayout from "@layouts/PlanTenantsLayout.vue";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ActionLists from "@components/elements/ulLists/ActionLists.vue";
+import PrimaryButton from "@components/elements/button/PrimaryButton.vue";
+import Modal from "@components/ui/modal/Index.vue";
 import ModalSidebar from "@components/ui/sidebar/ModalSidebar.vue";
-import CreateForm from '@pages/dashboard/central_page/subscription_page/tenant_plan_page/CreateForm.vue';
-import NotFound from '@components/ui/cta/NotFound.vue';
-import FormatCurrency from '@lib/Currency';
-import LoadingSkeleton from '@components/ui/loading/Skeleton.vue'
+import CreateForm from "@pages/dashboard/central_page/subscription_page/tenant_plan_page/CreateForm.vue";
+import NotFound from "@components/ui/cta/NotFound.vue";
+import FormatCurrency from "@lib/Currency";
+import LoadingSkeleton from "@components/ui/loading/Skeleton.vue";
 
 const LazyPlanDetail = defineAsyncComponent({
-    loader: () => import("./PlanDetail.vue")
-})
-
-const props = defineProps({
-    "getTenantPlanData": Array
+    loader: () => import("./PlanDetail.vue"),
 });
 
-const dataSubscriptions = toRef(() => props.getTenantPlanData)
-const createModalVisible = ref(false)
-const detailModalVisible = ref(false)
-const tenantPlanId = ref(null)
+const props = defineProps({
+    getTenantPlanData: Array,
+});
+
+const dataSubscriptions = toRef(() => props.getTenantPlanData);
+const createModalVisible = ref(false);
+const detailModalVisible = ref(false);
+const tenantPlanId = ref(null);
+const refreshDetailTenantSuspense = ref(false);
+const newVersionFormActive = ref(false);
 
 const openDetailPlan = (id) => {
     detailModalVisible.value = true;
     tenantPlanId.value = id;
-}
+};
 
+const changeDetailToFormSuspense = () => {
+    refreshDetailTenantSuspense.value = true;
+    newVersionFormActive.value = true;
+};
+
+watch(
+    () => refreshDetailTenantSuspense.value,
+    (newState) => {
+        if (newState) {
+            setTimeout(() => {
+                refreshDetailTenantSuspense.value = false;
+            }, 1000);
+        }
+    },
+);
 </script>
 
 <template>
     <PlanTenantsLayout>
         <section class="p-8">
-            <PrimaryButton icon="pi pi-plus" label="Add Plan Subscription"
-                @click-event="() => createModalVisible = true" />
+            <PrimaryButton
+                icon="pi pi-plus"
+                label="Add Plan Subscription"
+                @click-event="() => (createModalVisible = true)"
+            />
         </section>
 
-        <NotFound caption="Tenant plan is empty" v-if="dataSubscriptions.length <= 0" />
+        <NotFound
+            caption="Tenant plan is empty"
+            v-if="dataSubscriptions.length <= 0"
+        />
 
-        <section class="table_subscriptions" v-if="dataSubscriptions.length > 0">
-
-            <DataTable :value="dataSubscriptions" :pt="{
-                bodyrow: 'bg-transparent last:border-none border-b border-primary-700 odd:bg-primary-800',
-                column: {
-                    headercell: 'py-6 px-12 border-b border-primary-600',
-                    headercontent: 'text-left font-[300] tracking-wide text-white/50',
-                    bodycell: 'px-12 py-6 text-base',
-                }
-            }">
+        <section
+            class="table_subscriptions"
+            v-if="dataSubscriptions.length > 0"
+        >
+            <DataTable
+                :value="dataSubscriptions"
+                :pt="{
+                    bodyrow:
+                        'bg-transparent last:border-none border-b border-primary-700 odd:bg-primary-800',
+                    column: {
+                        headercell: 'py-6 px-12 border-b border-primary-600',
+                        headercontent:
+                            'text-left font-[300] tracking-wide text-white/50',
+                        bodycell: 'px-12 py-6 text-base',
+                    },
+                }"
+            >
                 <Column header="Plans">
                     <template #body="slotProps">
                         <div>{{ slotProps.data.name }}</div>
@@ -59,38 +96,67 @@ const openDetailPlan = (id) => {
                 <Column header="Price">
                     <template #body="slotProps">
                         <ul class="flex flex-col gap-y-4">
-                            <li>{{ FormatCurrency(slotProps.data.price_per_month) }}</li>
-                            <li>{{ FormatCurrency(slotProps.data.price_per_year) }}</li>
+                            <li>
+                                {{
+                                    FormatCurrency(
+                                        slotProps.data.price_per_month,
+                                    )
+                                }}
+                            </li>
+                            <li>
+                                {{
+                                    FormatCurrency(
+                                        slotProps.data.price_per_year,
+                                    )
+                                }}
+                            </li>
                         </ul>
                     </template>
                 </Column>
                 <Column header="Feature count">
                     <template #body="slotProps">
-                        <div>{{ slotProps.data.plan_features.length }} Features</div>
+                        <div>
+                            {{ slotProps.data.plan_features.length }} Features
+                        </div>
                     </template>
                 </Column>
                 <Column header="Version">
                     <template #body="slotProps">
-                        <div> v.{{ slotProps.data.plan_version }}</div>
+                        <div>v.{{ slotProps.data.plan_version }}</div>
                     </template>
                 </Column>
                 <Column header="Actions">
                     <template #body="slotProps">
-                        <ActionLists @detailEvent="openDetailPlan(slotProps.data.id)" />
+                        <ActionLists
+                            @detailEvent="openDetailPlan(slotProps.data.id)"
+                        />
                     </template>
                 </Column>
             </DataTable>
-
         </section>
-
     </PlanTenantsLayout>
 
-    <ModalSidebar :modal-visible="detailModalVisible" title="Plan detail" @close-modal="() => {
-        detailModalVisible = false
-        tenantPlanId.value = null
-    }">
-        <Suspense>
-            <LazyPlanDetail :id="tenantPlanId" />
+    <ModalSidebar
+        :modal-visible="detailModalVisible"
+        title="Plan detail"
+        @close-modal="
+            () => {
+                detailModalVisible = false;
+                tenantPlanId = null;
+                newVersionFormActive = false;
+            }
+        "
+    >
+        <Suspense v-if="!refreshDetailTenantSuspense">
+            <template #default>
+                <div v-if="!newVersionFormActive">
+                    <LazyPlanDetail
+                        :id="tenantPlanId"
+                        @new-version-event="changeDetailToFormSuspense"
+                    />
+                </div>
+                <div v-else></div>
+            </template>
 
             <template #fallback>
                 <LoadingSkeleton />
@@ -98,8 +164,11 @@ const openDetailPlan = (id) => {
         </Suspense>
     </ModalSidebar>
 
-    <Modal title="Create tenant subscription plan" :modal-visible="createModalVisible"
-        @close-modal="() => createModalVisible = false">
+    <Modal
+        title="Create tenant subscription plan"
+        :modal-visible="createModalVisible"
+        @close-modal="() => (createModalVisible = false)"
+    >
         <CreateForm />
     </Modal>
 </template>
--- resources/js/Pages/dashboard/central_page/subscription_page/tenant_plan_page/PlanDetail.vue
+++ resources/js/Pages/dashboard/central_page/subscription_page/tenant_plan_page/PlanDetail.vue
@@ -1,5 +1,5 @@
 <script setup>
-import DynamicSectionContent from "@components/ui/dynamicDetailContent/Index.vue"
+import DynamicSectionContent from "@components/ui/dynamicDetailContent/Index.vue";
 import axiosHttp from "@lib/axios";
 import { route } from "ziggy-js";
 import { onMounted, ref } from "vue";
@@ -7,8 +7,8 @@ import FormatCurrency from "@lib/Currency";
 import PrimaryButton from "@components/elements/button/PrimaryButton.vue";
 
 const props = defineProps({
-    id: String
-})
+    id: String,
+});
 
 const features = [
     "Booking personal trainer",
@@ -23,23 +23,24 @@ const features = [
     "Unlimited membership user",
 ];
 
-const planDetails = ref(null)
+const planDetails = ref(null);
+const emits = defineEmits(["newVersionEvent"]);
 
 const fetchDetailPlan = async () => {
     try {
-        const res = await axiosHttp(route('plan_tenant.json.detail', {
-            planTenant: props.id
-        }))
+        const res = await axiosHttp(
+            route("plan_tenant.json.detail", {
+                planTenant: props.id,
+            }),
+        );
 
         if (res.status == 200) {
             planDetails.value = await res.data;
         }
-
-
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+};
 
 await fetchDetailPlan();
 </script>
@@ -48,17 +49,36 @@ await fetchDetailPlan();
     <div class="grid my-8">
         <DynamicSectionContent label="title" :caption="planDetails.name" />
         <div class="flex items-center gap-x-14">
-            <DynamicSectionContent label="Price per month" :caption="FormatCurrency(planDetails.price_per_month)" />
-            <DynamicSectionContent label="Price per yearly" :caption="FormatCurrency(planDetails.price_per_year)" />
+            <DynamicSectionContent
+                label="Price per month"
+                :caption="FormatCurrency(planDetails.price_per_month)"
+            />
+            <DynamicSectionContent
+                label="Price per yearly"
+                :caption="FormatCurrency(planDetails.price_per_year)"
+            />
         </div>
-        <DynamicSectionContent label="Latest version" :caption="`v.${planDetails.plan_version}`" />
+        <DynamicSectionContent
+            label="Latest version"
+            :caption="`v.${planDetails.plan_version}`"
+        />
         <div class="mb-8">
             <h4 class="mb-4 text-base font-semibold capitalize">Features</h4>
-            <ul class="flex flex-col gap-x-14 gap-y-4 justify-between max-w-[400px]">
-                <li v-for="(feature, i) in planDetails.plan_features" :key="feature.id" class="text-base">{{
-                    feature.name }}</li>
+            <ul
+                class="flex flex-col gap-x-14 gap-y-4 justify-between max-w-[400px]"
+            >
+                <li
+                    v-for="(feature, i) in planDetails.plan_features"
+                    :key="feature.id"
+                    class="text-base"
+                >
+                    {{ feature.name }}
+                </li>
             </ul>
         </div>
-        <PrimaryButton label="Add new version" />
+        <PrimaryButton
+            label="Add new version"
+            @click-event="$emit('newVersionEvent')"
+        />
     </div>
 </template>
