0797cc33f96d053a6e8568dc444009df972edd06
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2024-06-30 19:12:15 +0800

    -m "resolve merge"

--- resources/js/Pages/landing_page/tenant_page/TenantHome.vue
+++ resources/js/Pages/landing_page/tenant_page/TenantHome.vue
@@ -6,7 +6,6 @@ import TenantLayout from "@layouts/TenantLandingPageLayout.vue";
 import Hero from "@components/tenant-pages/landingPage/Hero.vue";
 import CTA from "@components/tenant-pages/landingPage/CallToAction.vue";
 import Service from "@components/tenant-pages/landingPage/Service.vue";
-import Pricing from '@components/tenant-pages/landingPage/Pricing.vue'
 import Testimonials from "@components/tenant-pages/landingPage/Testimonials.vue";
 </script>
 
