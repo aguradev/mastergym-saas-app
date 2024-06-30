54b5d8761b67779a1b33cdf08473e6db6f5871bf
gungrages <gungrages>
2024-06-30 15:05:41 +0800

    feat: fix input text area

--- resources/js/Components/elements/input/InputTextArea.vue
+++ resources/js/Components/elements/input/InputTextArea.vue
@@ -1,5 +1,11 @@
 <script setup>
+import { toRef, watch } from "vue";
+
+const props = defineProps({
+    inputValue: String,
+});
 const emits = defineEmits(["update:inputValue"]);
+const inputValue = toRef(() => props.inputValue);
 const updateEventValue = (event) => {
     emits("update:inputValue", event.target.value);
 };
@@ -9,5 +15,6 @@ const updateEventValue = (event) => {
     <textarea
         class="bg-primary-700 rounded-lg p-4 outline-none h-[150px] resize-none"
         @input="updateEventValue"
+        :value="inputValue"
     ></textarea>
 </template>
