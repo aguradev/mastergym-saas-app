7837be77bd23c36b37c19a23fe73f19a3d2801e4
gungrages <gungrages>
2024-07-06 13:11:36 +0800

    feat: setup order summary

--- app/Http/Controllers/MainPlatform/Transaction/CheckoutController.php
+++ app/Http/Controllers/MainPlatform/Transaction/CheckoutController.php
@@ -10,19 +10,23 @@ class CheckoutController extends Controller
 {
     public function RedirectToCheckout(TenantSubscriptionPlan $tenantSubscriptionPlan)
     {
+
         if (is_null($tenantSubscriptionPlan)) {
             return redirect()->back();
         }
 
-        session('purchase_subscription_plan', $tenantSubscriptionPlan);
+
+        session()->put('purchase_subscription_plan', $tenantSubscriptionPlan);
         return to_route('transaction.checkout');
     }
 
     public function CheckoutPage()
     {
+
         if (!session()->has('purchase_subscription_plan')) {
             return to_route('central.landingPage');
         }
+
 
         return Inertia::render("landing_page/central_page/CheckoutTenant");
     }
--- resources/js/Pages/landing_page/central_page/CheckoutTenant.vue
+++ resources/js/Pages/landing_page/central_page/CheckoutTenant.vue
@@ -3,6 +3,8 @@ import { ref } from "vue";
 import { Head, Link } from "@inertiajs/vue3";
 import SubscriptionInfo from "@components/central-pages/landingPage/SubscriptionInfo.vue";
 import GymIcon from "/public/assets/images/icon/gym.png";
+import FormatCurrency from "../../../Lib/Currency";
+import FeatLists from "@components/ui/pricing-card/FeatLists.vue";
 
 const items = { name: "Ultimate Subscription", duration: 6, price: 1000000 };
 </script>
@@ -14,47 +16,59 @@ const items = { name: "Ultimate Subscription", duration: 6, price: 1000000 };
 </style>
 
 <template>
+    <Head title="Checkout" />
 
-    <Head title="Payment Page" />
-    <div class="absolute p-10 hidden md:block flex">
-        <img :src="GymIcon" alt="gym logo" class="w-[70px] inline-block" />
-    </div>
-    <form @submit.prevent="submit">
-        <div id="background_drop"
-            class="w-screen h-screen bg-[url('/public/storage/images/svg/stacked-waves-haikei2.svg')] flex justify-center py-10">
-            <div class="card shadow-xl bg-white flex-row w-11/12 md:w-2/5 rounded-2xl text-black p-5">
-                <div id="title"
-                    class="bg-blue-400 shadow-md border-b-4 border-blue-200 rounded-xl h-1/6 text-center pt-7 md:pt-9">
-                    <p class="text-3xl font-semibold drop-shadow-md text-white">
-                        Gym Master - Payment
-                    </p>
-                </div>
-                <div id="user" class="h-1/6 p-5 leading-6 bord">
-                    <p class="text-xl font-medium">Michael Jackson Gumembert</p>
-                    <p>useremail@gmail.com</p>
+    <div class="h-screen">
+        <div class="grid lg:grid-cols-2 gap-4">
+            <!-- user form -->
+            <div></div>
+            <!-- product info -->
+            <div
+                class="bg-primary-800 py-10 px-8 h-screen border-l border-surface-600"
+            >
+                <h2 class="text-3xl font-semibold mb-8">Order Summary</h2>
+                <div
+                    class="bg-primary-800 p-4 rounded-lg border border-surface-600"
+                >
+                    <ul class="flex flex-col gap-6">
+                        <li class="flex flex-col gap-2 text-lg">
+                            <span class="font-semibold">Purchase plan</span>
+                            <span>Premium</span>
+                        </li>
+                        <li class="flex flex-col gap-2 text-lg">
+                            <span class="font-semibold">Feature</span>
+                            <FeatLists
+                                class="!space-y-2 !mt-1"
+                                list-class="!text-white"
+                            />
+                        </li>
+                        <li class="flex flex-col gap-2 text-lg">
+                            <span class="font-semibold">Period Type</span>
+                            <span>Monthly</span>
+                        </li>
+                    </ul>
                 </div>
 
-                <SubscriptionInfo :name="items.name" :duration="items.duration" :price="items.price" id="subscription"
-                    class="h-2/6 bord">
-                </SubscriptionInfo>
-                <div id="title" class="h-1/6 text-start pt-3 md:pt-8 px-2 md:px-10 bord">
-                    <div>
-                        <input class="inline-block mr-2" type="checkbox" />
-                        <label class="text-sm">I Confirm that the information displayed above is
-                            true.</label>
-                    </div>
-                    <div>
-                        <input class="inline-block mr-2" type="checkbox" />
-                        <label class="text-sm">I Agree to the Terms and Condition of the Gym
-                            Master Service</label>
-                    </div>
-                </div>
-                <div id="payment" class="flex justify-center items-center h-1/6">
-                    <Link href=""
-                        class="inline-block shadow-xl rounded-md font-medium text-white bg-green-500 py-2 px-5 md:px-10 h-fit">
-                    Proceed to the Payment Process</Link>
-                </div>
+                <hr class="my-8 border-t border-t-surface-600" />
+
+                <ul class="flex flex-col gap-4">
+                    <li class="flex justify-between gap-2 text-lg">
+                        <span class="font-semibold">Price</span>
+                        <span>{{ FormatCurrency(3000000) }}</span>
+                    </li>
+                    <li class="flex justify-between gap-2 text-lg">
+                        <span class="font-semibold">Tax</span>
+                        <span>10%</span>
+                    </li>
+                </ul>
+
+                <ul class="mt-8">
+                    <li class="flex justify-between gap-2 text-lg">
+                        <span class="font-semibold">Total</span>
+                        <span>{{ FormatCurrency(3540000) }}</span>
+                    </li>
+                </ul>
             </div>
         </div>
-    </form>
+    </div>
 </template>
