d5c73e159802c097d6ded89227a51f734b1b8829
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2024-08-06 21:16:00 +0800

    fix/remove overview page and adjusting cta form layout

--- resources/js/Stores/content_menus.js
+++ resources/js/Stores/content_menus.js
@@ -3,7 +3,6 @@ import { ref } from 'vue';
 
 export const useContentMenus = defineStore('content_menus', () => {
     const contentMenus = ref([
-        { title: 'Overview', url: route('website.overview') },
         { title: 'Hero', url: route('website.hero') },
         { title: 'CTA', url: route('website.cta') },
         { title: 'Service', url: route('website.service') },
