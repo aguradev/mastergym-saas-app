c1861843702aa9993762bb3895569c02d3def58e
gungrages <gungrages>
2024-08-24 13:04:40 +0800

    feat: chart

--- resources/js/Pages/dashboard/tenant_page/dashboard_overview/StaffOverview.vue
+++ resources/js/Pages/dashboard/tenant_page/dashboard_overview/StaffOverview.vue
@@ -1,11 +1,74 @@
 <script setup>
-import { toRef } from "vue";
+import { onMounted, ref, toRef } from "vue";
 import { usePage } from "@inertiajs/vue3";
+import Chart from "primevue/chart";
 
 const page = usePage();
 const totalStaff = toRef(() => page.props?.totalStaff);
 const totalTrainee = toRef(() => page.props?.totalTrainee);
 const totalMembershipPlan = toRef(() => page.props?.totalMembershipPlan);
+
+const chartData = ref(() => {});
+const chartOptions = ref(() => {});
+
+onMounted(() => {
+    chartOptions.value = () => {
+        const documentStyle = getComputedStyle(document.documentElement);
+
+        return {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: "#FFF",
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: "#FFF",
+                    },
+                    grid: {
+                        color: "rgb(01 183 65)",
+                    },
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: "rgb(01 183 65)",
+                    },
+                    grid: {
+                        color: "transparent",
+                    },
+                },
+            },
+        };
+    };
+    chartData.value = () => {
+        return {
+            labels: ["Q1", "Q2", "Q3", "Q4"],
+            datasets: [
+                {
+                    label: "Sales",
+                    data: [540, 325, 702, 620],
+                    backgroundColor: [
+                        "rgba(249, 115, 22, 0.2)",
+                        "rgba(6, 182, 212, 0.2)",
+                        "rgb(107, 114, 128, 0.2)",
+                        "rgba(139, 92, 246 0.2)",
+                    ],
+                    borderColor: [
+                        "rgb(249, 115, 22)",
+                        "rgb(6, 182, 212)",
+                        "rgb(107, 114, 128)",
+                        "rgb(139, 92, 246)",
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        };
+    };
+});
 </script>
 
 <template>
@@ -33,5 +96,11 @@ const totalMembershipPlan = toRef(() => page.props?.totalMembershipPlan);
     </div>
     <div class="bg-primary-800 border border-surface-500 p-5 rounded-lg">
         <h4 class="font-semibold text-xl mb-4">Income</h4>
+        <Chart
+            type="bar"
+            :data="chartData()"
+            :options="chartOptions()"
+            :height="100"
+        />
     </div>
 </template>
